12b16c1adcd24277a6984c63f2f30f6aa6bd7283
Jonathan Zavala <jontzavala@gmail.com>
2021-12-04 17:17:16 -0600

    added reducer and added store to provider

---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+const { Switch } = require("react-router-dom");
+
+function reducer(state = {courses: [], students: [], behaviors: []}, action){
+    switch (action.type) {
+        // case value:
+            
+        //     break;
+    
+        default:
+            return state;
+    }
+}
+
+export default reducer;
