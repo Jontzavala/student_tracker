64b5cc170a3541ee0a77bc779464aac4c62c9f20
Jonathan Zavala <jontzavala@gmail.com>
2021-12-04 17:46:38 -0600

    added NavLink

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { NavLink } from 'react-router-dom'
 import Pages from './components/Pages'
 
 function App() {
   return (
     <div className="App">
+      <NavLink to="/">Home</NavLink>
+      <br />
+      <NavLink to="/courses">Courses</NavLink>
+      <br />
       <Pages />
     </div>
   );
